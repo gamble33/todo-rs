33d2eb84bd9656c12a43109699861a8d4bec51dc
Slopey <giafilmsyt@gmail.com>
2022-03-14 11:54:16 +0100

    init commit

---
+++ src/lib.rs
@@ -0,0 +1,78 @@
+use std::io::{Stdout, Write};
+use termion::raw::{RawTerminal};
+use std::fs::File;
+
+#[derive(Clone)]
+pub struct Todo<'a> {
+    pub text: &'a str,
+    pub completed: bool,
+    pub starred: bool,
+}
+
+impl<'a> Todo<'a> {
+    pub fn new(text: &'a str) -> Todo<'a> {
+        Todo {
+            text,
+            completed: false,
+            starred: false
+        }
+    }
+}
+
+pub fn read_todos<'a>(path: &str) -> Vec<Todo<'a>> {
+    let mut todos: Vec<Todo<'a>> = Vec::new();
+    let string = std::fs::read_to_string(path).unwrap();
+    let a_str: &'a str = string.as_str();
+    let split_string = a_str.split::<'a>('~');
+    let mut v: Vec<Todo<'a>> = Vec::new();
+    for val in split_string {
+        v.push(Todo::<'a>::new(val));
+    }
+    return v;
+}
+
+pub fn write_todos(path: &str, todos: &[Todo]) {
+    let mut f_text: String = String::new();
+    todos.iter().for_each(|todo| {
+        f_text.push_str(todo.text);
+        f_text.push('~');
+        f_text.push(match todo.completed {
+            true => '1',
+            false => '0'
+        });
+        f_text.push('~');
+        f_text.push(match todo.starred {
+            true => '1',
+            false => '0'
+        });
+    });
+    std::fs::write(path, f_text.as_str());
+}
+
+pub fn render_todos(stdout: &mut RawTerminal<Stdout>, todos: &[Todo], todo_curr: usize){
+    for (index, todo) in todos.iter().enumerate() {
+        write!(stdout,
+                "{}{}{}",
+                termion::cursor::Down(1),
+                termion::cursor::Left(u16::MAX),
+                termion::style::Reset
+        ).unwrap();
+        
+        if todo.starred { write!(stdout, "{}", termion::color::Fg(termion::color::Yellow)).unwrap(); }
+        if todo.completed { write!(stdout, "[ X ]").unwrap(); } else { write!(stdout, "[   ]").unwrap(); }
+
+        if index == todo_curr {
+            write!(stdout,
+                "{}{}",
+                termion::style::Underline,
+                termion::style::Invert
+            ).unwrap();
+        }
+
+        write!(stdout,
+            "{}",
+            todo.text
+        ).unwrap();
+       
+    }
+}
---
+++ src/main.rs
@@ -0,0 +1,74 @@
+use termion::raw::{IntoRawMode, RawTerminal};
+use termion::input::TermRead;
+use termion::event::Key;
+use std::io::{stdout, stdin, Stdout, Write};
+use todo_rs::Todo;
+
+fn main() {
+
+    let f = match std::fs::File::open("todos.todo") {
+        Ok(file) => file,
+        Err(_) => match std::fs::File::create("todos.todo") {
+            Ok(file) => file,
+            Err(e) => panic!("{}", e)
+        }
+    };
+
+    let stdin = stdin();
+    let mut stdout: RawTerminal<Stdout> = stdout().into_raw_mode().unwrap();
+
+    let mut todos: Vec<Todo> = vec![
+        Todo::new("Clean Dishes"),
+        Todo::new("Buy a Bread"),
+        Todo::new("Switch to Kali Linux"),
+        Todo::new("Make to-do app"),
+    ];
+
+    write!(stdout,
+        "{}{}[q] or Ctrl+C to exit.{}",
+        termion::clear::All,
+        termion::cursor::Goto(1, 1),
+        termion::cursor::Hide
+    ).unwrap();
+
+    stdout.flush().unwrap();
+
+    let mut todo_curr: usize = 2;
+
+    for c in stdin.keys() {
+    
+        match c.unwrap() {
+            Key::Char('q') => break,
+            Key::Ctrl('c') => break,
+
+            Key::Up => {
+                if todo_curr > 0 { todo_curr -= 1; }
+            },
+
+            Key::Down => {
+                todo_curr += 1;
+            },
+
+            Key::Char('\n') => {
+                todos[todo_curr].completed = !todos[todo_curr].completed;
+            }
+
+            Key::Char('s') => {
+                todos[todo_curr].starred = !todos[todo_curr].starred;
+            }
+            _ => {}
+        }
+
+        if todo_curr >= todos.len() { todo_curr = todos.len()-1; }
+
+        write!(stdout, "{}{}", termion::clear::All, termion::cursor::Goto(1,1)).unwrap();
+
+        todo_rs::render_todos(&mut stdout, todos.as_slice(), todo_curr); 
+
+        stdout.flush().unwrap();
+    }
+    
+    todo_rs::write_todos("todos.todo", todos.as_slice());   
+
+    write!(stdout, "{}", termion::cursor::Show).unwrap();
+}
